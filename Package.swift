e2a9a401346300d0182640e398e47285e195b825
Alexander Zemlyanukhin <alex.zemlyanukhin@meshconnect.com>
2024-11-22 12:11:59 +0500

    3.0.4

--- Package.swift
+++ Package.swift
@@ -16,8 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LinkSDK",
-            url: "https://github.com/FrontFin/mesh-ios-sdk/releases/download/latest/LinkSDK.xcframework.zip",
-            checksum: "27dce2553ae71bff57fc2f343b45a85313111463d87d67294dde75506a2ac3da"
+            path: "LinkSDK.xcframework"
         )
     ]
 )
