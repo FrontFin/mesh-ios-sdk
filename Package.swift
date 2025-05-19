2a0ac0edb324051caa7797c96519cac74739b164
Alexander Zemlyanukhin <alex.zemlyanukhin@meshconnect.com>
2025-05-19 09:10:52 +0200

    build fix

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
             targets: ["LinkSDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/FrontFin/mesh-ios-sdk/releases/download/3.0.16/LinkSDK.xcframework.zip")
+        .package(url: "git@github.com:atomicfi/quantum-ios.git", exact: "3.12.0")
     ],
     targets: [
         .binaryTarget(
             name: "LinkSDK",
             url: "https://github.com/FrontFin/mesh-ios-sdk/releases/download/3.0.16/LinkSDK.xcframework.zip",
-            checksum: "791858e6e75bb2d67140a0cc751d9201d1759610bfa5ab97facb0a5392db4366"
+            checksum: "47d3a279cc877d8c922806482a122110325504f75855c9760f4590a978bd2d0e"
         )
     ]
 )
